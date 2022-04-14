9879c00c871a00d6402e1b7dd85ba7f36b793ac6
Estrela Espiridião <estrelaespiridiao@gmail.com>
2022-04-14 21:43:16 +0100

    Update betterThanAverage.js

--- 8kyu/betterThanAverage.js
+++ 8kyu/betterThanAverage.js
@@ -1,17 +1,13 @@
 function betterThanAverage(classPoints, yourPoints) {
-  console.log("hello");
-  let sum = 0;
-  for (let i = 0; i < classPoints.length; i++) {
-    sum += classPoints[i];
-  }
-  console.log(sum);
-  let average = sum + yourPoints / (classPoints.length + 1);
-  console.log(average);
-  if (yourPoints > average) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
-}
-
-betterThanAverage([2, 3], 5);
+    let sum = 0;
+     classPoints.push(yourPoints)
+     for (let i = 0; i < classPoints.length; i++) {
+       sum += classPoints[i];
+     }
+     if (yourPoints > sum / classPoints.length) {
+       return true
+     } else {
+       return false
+     }
+   }
+   
