0755ebb918b4c19f0f514cf0dc3910084073ec19
Estrela Espiridião <estrelaespiridiao@gmail.com>
2022-06-14 14:14:13 +0100

    Create stringEndsWith.js

---
+++ 7kyu/stringEndsWith.js
@@ -0,0 +1,12 @@
+function solution(str, ending){
+  if(str.endsWith(ending)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+//other solutions
+function solution(str, ending){
+  return str.endsWith(ending);
+}
