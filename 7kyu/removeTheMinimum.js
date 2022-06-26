bc0a1c0126ff4f0b196e46c5c0daf562131b3442
Estrela Espiridião <estrelaespiridiao@gmail.com>
2022-06-26 23:54:07 +0100

    Update removeTheMinimum.js

--- 7kyu/removeTheMinimum.js
+++ 7kyu/removeTheMinimum.js
@@ -6,3 +6,15 @@ function removeSmallest(numbers) {
   return result
 
 }
+//SECOND TRYYYY
+function removeSmallest(numbers) {
+  const min = Math.min(...numbers);
+  const index = numbers.indexOf(min)
+  return numbers.filter((e,i) => i !== index)
+}
+
+//other solutions
+function removeSmallest(numbers) {
+  let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
